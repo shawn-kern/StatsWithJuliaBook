abe2e28de78ca07d56218ac363c00b1c5a7080c1
shawn-kern <shawn.kern@kern.site>
2022-06-27 11:25:51 -0700

    refactored find routines to produce sorted df

--- hfacs/process_data.jl
+++ hfacs/process_data.jl
@@ -35,8 +35,18 @@ hfacs_data = hfacs_data[:, Not([:NanoCode, :Days, :Cost])]
 unique!(hfacs_data)
 hfacs_data = innerjoin(hfacs_data, costs, on=:ID)
 
-#nanocode_index = SortedSet(union(nanocodes.codes...))
 nanocode_index = sort(collect(union(nanocodes.codes...)))
+
+nanocode_names = Dict()
+for row in eachrow(unique(raw_data[:, [:NanoCode, :NanoCode_Name]]))
+    nanocode_names[row.NanoCode] = row.NanoCode_Name
+end
+
+frequency=Dict()
+for code in nanocode_index
+    frequency[code, "All"] = sum(hfacs_data[:, code])
+    frequency[code, "Armstrong"] = sum(hfacs_data[hfacs_data.Center .== "Armstrong", code])
+end
 
 codes = Array{Bool, 2}(undef, nrow(nanocodes), length(nanocode_index))
 for (i, code) in enumerate(nanocode_index)
@@ -44,22 +54,10 @@ for (i, code) in enumerate(nanocode_index)
 end
 hfacs_data = hcat(hfacs_data, DataFrame(codes, collect(nanocode_index)))
 
-# simple analyses
+# analyses
 for level in levels(hfacs_data.Type)
     println("Mishap totals for $level: all centers $(sum(hfacs_data.Type .== level)), Armstrong $(sum(hfacs_data.Type[hfacs_data.Center .== "Armstrong"] .== level))")
 end
-
-frequency=Dict()
-for code in nanocode_index
-    frequency[code]=(sum(hfacs_data[:, code]), sum(hfacs_data[hfacs_data.Center .== "Armstrong", code]))
-end
-
-#= full_matrix = [ dot(hfacs_data[:, nanocode_index[i]], hfacs_data[:, nanocode_index[j]]) 
-    for i in 1:length(nanocode_index), j in 1:length(nanocode_index) ]
-
-Armstrong_matrix = [ dot(hfacs_data[hfacs_data.Center .== "Armstrong", 
-    nanocode_index[i]], hfacs_data[hfacs_data.Center .== "Armstrong", nanocode_index[j]]) 
-    for i in 1:length(nanocode_index), j in 1:length(nanocode_index) ] =#
 
 X2_pvalues = [ pvalue(ChisqTest(freqtable(hfacs_data[:, i], hfacs_data[:,j])), tail=:right)
      for i in nanocode_index, j in nanocode_index ]
@@ -81,39 +79,30 @@ function categorical_correlation(index1, index2)
     return V, X2, pvalue
 end
 
-
 # find correlated pairs of nanocodes that occur more than once
-function find_index_pairs(threshold=0.5, center="All")
-    count = 0   
-    index = center=="Armstrong" ? 2 : 1
+function find_index_pairs(; α=0.05, center="All", min_n=1)
+    results = DataFrame(Index1=Symbol[], Index2=Symbol[], V=Float64[], pvalue=Float64[], N1=Int[], N2=Int[])
     for i in nanocode_index
         for j in nanocode_index
             if i ≥ j continue end
             V, X2, pvalue = categorical_correlation(i, j)
-            if V > threshold && (frequency[i][index] > 1 && frequency[j][index] > 1)
-                count += 1
-                println("$i & $j have $V, $pvalue, ($(frequency[i][index]), $(frequency[j][index]))")
+            if pvalue < α && (frequency[i, center] > min_n && frequency[j, center] > min_n)
+                push!(results, (i, j, V, pvalue, frequency[i, center], frequency[j, center]))
             end
         end
     end
-    println(count)
+    return sort(results, [:pvalue])
 end
 
-function find_mishap_index(threshold=0.5, center="All", type="Damage") 
-    count = 0   
-    index = center=="Armstrong" ? 2 : 1
-    Type = type == "Damage" ? :Damage : :Injury
-    for i in nanocode_index
-        V, X2, pvalue = categorical_correlation(i, Type)
-        if V > threshold && frequency[i][index] > 1
-                count += 1
-                println("$i & $Type have $V, $pvalue, ($(frequency[i][index]))")
+# find nanocodes correlated with specific index
+function find_index_match(index=:Type; α=0.05, center="All", min_n=1)
+    results = DataFrame(Index1=Symbol[], Index2=Symbol[], V=Float64[], pvalue=Float64[], N=Int[])
+    for j in nanocode_index
+        if j == index continue end
+        V, X2, pvalue = categorical_correlation(index, j)
+        if pvalue < α && frequency[j, center] > min_n
+            push!(results, (index, j, V, pvalue, frequency[j, center]))
         end
     end
-    println(count)
-end
-
-nanocode_names = Dict()
-for row in eachrow(unique(raw_data[:, [:NanoCode, :NanoCode_Name]]))
-    nanocode_names[row.NanoCode] = row.NanoCode_Name
+    return sort(results, [:pvalue])
 end
